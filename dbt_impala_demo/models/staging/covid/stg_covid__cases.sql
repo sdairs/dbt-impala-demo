4f4a27557f447e8eec384224245509eab53ca5b2
Alasdair Brown <sdairs@users.noreply.github.com>
2022-05-16 23:57:44 +0100

    Delete stg_covid__cases.sql

--- dbt_impala_demo/models/staging/covid/stg_covid__cases.sql
+++
@@ -1,6 +0,0 @@
-select 
-  cast(cast(unix_timestamp(date_rep, "dd/MM/yyyy") as timestamp) AS DATE) as date_rep, 
-  cast(cases as BIGINT) as cases, 
-  cast(deaths as BIGINT) as deaths, 
-  geo_id
-from {{ source('raw_covid','raw_covid__cases') }};
